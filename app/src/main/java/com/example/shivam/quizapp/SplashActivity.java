6350c544be94764bf03e6643ab134e517dbaffd9
ShivamTaneja <shivamtaneja2211997@gmail.com>
2018-07-06 15:25:25 +0530

    initial commit

---
+++ app/src/main/java/com/example/shivam/quizapp/SplashActivity.java
@@ -0,0 +1,24 @@
+package com.example.shivam.quizapp;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.SystemClock;
+import android.support.v7.app.AppCompatActivity;
+
+import java.util.concurrent.TimeUnit;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        SystemClock.sleep(TimeUnit.SECONDS.toMillis(1));
+
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
